18fd9ccd1c033cd31936d0004f49ceea89da6bfb
Vansh <vsaboo30@stuy.edu>
2023-05-15 20:36:52 -0400

    Converting DB to Dictionary to JSON, finally

--- proof_of_concept/backend.py
+++ proof_of_concept/backend.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify  # facilitate flask webserving
+from flask import Flask  # facilitate flask webserving
 from flask import render_template  # facilitate jinja templating
 from flask import request  # facilitate form submission
 from flask import session
 from flask import Response
 import sqlite3, requests, os, json, datetime
 import pandas as pd
+import os
+import csv
+import json
 
 app = Flask(__name__)
 
@@ -13,19 +16,50 @@ DB_FILE = "sales.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
 
-# c.execute('''.mode json''')
 
+# DB --> Dictionary --> JSON
 @app.route("/", methods=["GET", "POST"])
 def returnData():
-    # data = request.get_json() #fix
-    # data = jsonify(data) #fix
-    # data = json.dump('sales.db')
-    # data = c.execute('SELECT * FROM video_games')
-    data = c.execute('''SELECT json_group_array(json_object('game_name', game_name, 'platform', platform)) AS json_result
-FROM (SELECT * FROM video_games)''')
+    temp = "Wii" # Temporary for now, will use other platforms later based on user selection
+    data = c.execute("SELECT * FROM video_games WHERE Platform=?", (temp, ))
+    results = c.fetchall()
     print("ran")
-    print(data)
+    print(results)
+    features = []
+    for row in results:
+        feature = {
+            "Rank": row[0],
+            "Name": row[1],
+            "Platform": row[2],
+            "Year": row[3],
+            "Genre": row[4],
+            "Publisher": row[5],
+            "NA_Sales": row[6],
+            "EU_Sales": row[7], 
+            "JP_Sales": row[8],
+            "Other_Sales": row[9],
+            #"Global_Sales": row[10], # Create the Global_Sales in the DB later
+        }
+        features.append(feature)
+
+    print(features)
+
+    #gamejson = {"type": "FeatureCollection", "features": []}
+
+    #gamejson["features"] = features
+
+    with open("static/thing.json", "w") as f:
+        f.write(json.dumps(features[0]))
+
+    print("done")
+    
     return render_template('test.html')
+
+
+
+
+
+
 
 if __name__ == "__main__":  # false if this file imported as module
     # enable debugging, auto-restarting of server when this file is modified
--- proof_of_concept/static/test.js
+++ proof_of_concept/static/test.js
@@ -1,98 +1,27 @@
-// https://www.makeuseof.com/tag/python-javascript-communicate-json/
+// import data from 'thing.json' assert { type: 'json' };
 
-var sqlite3 = require('sqlite3').verbose();
+// <!--./index.js-->
 
-console.log("js file is correctly connected to the html file")
-
-para = document.getElementById("para")
-
-function createDatabase(file){
-    var db = new sqlite3.Database(file);
-    if(!fs.existsSync(file)){
-      console.log("creating database file");
-      fs.openSync(file, "w");
-      db.run("SELECT * from video_games");
-      
-      console.log("database initialized");
-    }
-  
-    return db;
-  }
-
-  db = createDatabase('../sales.db');
-  console.log(db);
 /*
-// fetch('http://127.0.0.1:3000')
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(users => {
-//         console.log(users);
-//     })
+fetch('thing.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json))
 */
+
+
 
 // async function logJSONData() {
-//     // const response = await fetch("http://127.0.0.1:3000");
-//     // const jsonData = await response.json();
-//     // console.log(jsonData);
-//     // para.innerHTML = jsonData
-//     let url = "http://127.0.0.1:5000";
-//     let request = new Request(url, { 
-//         method: 'get'
-//     });
-
-//     fetch(request)
-//         .then((response)=>{
-//             if(response.ok){
-//                 console.log(response.text())
-//                 return response.text();
-//             } else{
-//                 throw new Error("Somethin's wrong");
-//             }
-//         })
-//         .then((textData) =>{            
-            
-//             // para.innerHTML = textData;
-//             console.log(textData);
-//         }).catch((error) => {
-//             console.log('Error occured', error);
-//         });
-        
-//     }
-    //  http://127.0.0.1:3000
-
-      
-/*
-// function callAPI() {
-
-//     <strong>// Get the reciever endpoint from Python using fetch</strong>:
-//     fetch("http://127.0.0.1:5000/receiver", 
-//         {
-//             method: 'POST',
-//             headers: {
-//             'Content-type': 'application/json',
-//             'Accept': 'application/json'
-//             },
-//         body:JSON.stringify(cars)}).then(res=>{
-//             if(res.ok){
-//                 return res.json()
-//             }else{
-//                 alert("something is wrong")
-//             }
-//             }).then(jsonResponse=>{
-
-
-//             console.log(jsonResponse)
-//             } 
-//             ).catch((err) => console.error(err));
+//   const response = await fetch("thing.json");
+//   const jsonData = await response.json();
+//   console.log(jsonData);
+//   console.log("ran");
 // }
 
-// fetch("http://127.0.0.1:5000/receiver",
+// logJSONData();
 
-// {method: ''}
 
-// )
-*/
+// console.log(data);
 
-// para.innerHTML = logJSONData();
-// para.innerHTML = JSON.parse(logJSONData());
+// "thing.json"
+var parseData = JSON.parse(thing)
+console.log(parseData)
